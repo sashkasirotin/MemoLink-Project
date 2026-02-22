d358413890f7c55a5787b31b3e002254e902c68f
Ronbi1 <bitonr7@gmail.com>
2026-02-22 12:08:44 +0200

    boardgame css is updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@mantine/core/styles.css';
 import './index.css'
 import App from './App.jsx'
 import { MantineProvider } from '@mantine/core';
@@ -8,7 +9,12 @@ import { BrowserRouter } from 'react-router';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <MantineProvider>
+      <MantineProvider
+        theme={{
+          fontFamily: "Inter, system-ui, -apple-system, Segoe UI, Roboto, Arial, sans-serif",
+          headings: { fontFamily: "Inter, system-ui, -apple-system, Segoe UI, Roboto, Arial, sans-serif" },
+        }}
+      >
         <App />
       </MantineProvider>
     </BrowserRouter>
